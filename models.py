112d044d654b2d0db7b5226b255ba29a36c035ff
Ryan O'Donnell <odonnellryanc@gmail.com>
2015-10-27 09:47:10 -0400

    SQLAlchemy and Flask-Login
    Adding some more boilerplate

--- models.py
+++ models.py
@@ -1,11 +1,28 @@
-from peewee import SqliteDatabase, CharField, Model
+from flask.ext.sqlalchemy import SQLAlchemy
+from app import db
 
-db = SqliteDatabase('test.db')
 
-class BaseModel(Model):
-    class Meta:
-        database = db # This model uses the "people.db" database.
+class User(db.Model):
 
-class Person(Model):
-    name = CharField()
+    __tablename__ = 'user'
 
+    name = db.Column(db.String, nullable=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    authenticated = db.Column(db.Boolean, default=False)
+
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.email
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
